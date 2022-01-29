d5b45e2bdc22f767c65f2cc50926599775ab0d9f
241199 <241199@edu.p.lodz.pl>
2022-01-29 18:32:08 +0100

    Nowa Metoda Policz

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
+using namespace std;
+int Policz(int a,int b)
+{
+    return a+b;
+}
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    cout<<Policz(4,5);
     return 0;
 }
