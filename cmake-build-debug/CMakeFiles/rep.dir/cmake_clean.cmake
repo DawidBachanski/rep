fed004e774836e0c17dd89877fa2845b52732f25
241199 <241199@edu.p.lodz.pl>
2022-01-29 18:26:37 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/rep.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rep.dir/main.cpp.obj"
+  "librep.dll.a"
+  "rep.exe"
+  "rep.exe.manifest"
+  "rep.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rep.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
